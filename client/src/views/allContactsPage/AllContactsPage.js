0ca72795f2c43290c96768c6ca31024b12793b8f
Andrew <armcburn@gmail.com>
2022-04-25 15:05:13 -0400

    Add placeholder pages: Home, View Contact, Edit Contact, All Contacts

---
+++ client/src/views/allContactsPage/AllContactsPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AllContactsPage = () => {
+  return <div>AllContactsPage</div>;
+};
---
+++ client/src/views/editContactPage/EditContactPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const EditContactPage = () => {
+  return <div>EditContactPage</div>;
+};
