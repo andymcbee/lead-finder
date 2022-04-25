01c4537ded94d13aee218c7be62b38259131bf5e
Andrew <armcburn@gmail.com>
2022-04-25 16:28:32 -0400

    Add create contact form component

--- client/src/views/homePage/HomePage.js
+++ client/src/views/homePage/HomePage.js
@@ -1,5 +1,10 @@
 import React from "react";
+import FormNewContact from "../../components/formNewContact/FormNewContact";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="HomePage">
+      <FormNewContact />
+    </div>
+  );
 };
