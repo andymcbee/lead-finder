00f01642c74852a590a6b84010af363c47d51bae
Andrew <armcburn@gmail.com>
2022-04-25 15:29:51 -0400

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from "./views/homePage/HomePage";
+import { AllContactsPage } from "./views/allContactsPage/AllContactsPage";
+import { EditContactPage } from "./views/editContactPage/EditContactPage";
+import { SingleContactPage } from "./views/singleContactPage/SingleContactPage";
+import { TopBar } from "./components/topbar/TopBar";
 
 function App() {
-  return <div className="App">Home</div>;
+  return (
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contacts" element={<AllContactsPage />} />
+
+        <Route path="/contact/:contactId" element={<SingleContactPage />} />
+        <Route path="/edit-contact/:contactId" element={<EditContactPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/views/editContactPage/EditContactPage.js
+++ client/src/views/editContactPage/EditContactPage.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export const EditContactPage = () => {
+  const { contactId } = useParams();
+  console.log(contactId);
   return <div>EditContactPage</div>;
 };
