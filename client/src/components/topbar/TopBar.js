3651e42c69bdce7d19f6f96adec3d15fe781df5a
Andrew <armcburn@gmail.com>
2022-04-25 15:50:19 -0400

    Add top navbar with links

--- client/src/components/topbar/TopBar.js
+++ client/src/components/topbar/TopBar.js
@@ -1,5 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./topBar.css";
 
 export const TopBar = () => {
-  return <div>TopBar</div>;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <Link className="link topLeftTitle" to="/">
+          Lead Finder App
+        </Link>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/contacts">
+              Contacts
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/register">
+              Register
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
