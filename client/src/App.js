b98fe6f833ea970ed588cbef2a3a3a87f3cfc731
Andrew <armcburn@gmail.com>
2022-04-27 12:43:06 -0400

    Add protected routes based on JWT login status

--- client/src/App.js
+++ client/src/App.js
@@ -9,20 +9,38 @@ import UserSignup from "./views/userSignupPage/UserSignupPage";
 import UserSignin from "./views/userSigninPage/UserSigninPage";
 
 function App() {
+  const user = JSON.parse(localStorage.getItem("profile"));
+  console.log(user);
+
   return (
     <Router>
       <TopBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/contacts" element={<AllContactsPage />} />
 
-        <Route path="/contact/:contactId" element={<SingleContactPage />} />
-        <Route path="/edit-contact/:contactId" element={<EditContactPage />} />
-        <Route path="/signup" element={<UserSignup />} />
-        <Route path="/login" element={<UserSignin />} />
+        <Route
+          path="/contacts"
+          element={user ? <AllContactsPage /> : <UserSignup />}
+        />
+
+        <Route
+          path="/contact/:contactId"
+          element={user ? <SingleContactPage /> : <UserSignup />}
+        />
+
+        <Route
+          path="/edit-contact/:contactId"
+          element={user ? <EditContactPage /> : <UserSignup />}
+        />
+
+        <Route path="/signup" element={user ? <HomePage /> : <UserSignup />} />
+
+        <Route path="/login" element={user ? <HomePage /> : <UserSignin />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+//        <Route path="/signup" element={<UserSignup />} />
